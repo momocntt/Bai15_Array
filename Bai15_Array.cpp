98fe109056338d4aab26c7b597889ae378449be5
momocntt <nguyenthihoaimo1207@gmail.com>
2024-11-18 14:37:05 +0700

    Add project files.

---
+++ Bai15_Array.cpp
@@ -0,0 +1,17 @@
+// Bai15_Array
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Create an array of strings 
+	string cars[5] = { "Volvo","BMW","Ford","Mazda","Tesla" };
+	// Loop through strings
+	for (int i = 0; i < 5; i++) {
+		cout << cars[i] << "\n";
+	}
+}
+
+
